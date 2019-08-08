fa50a4c812f28c812033db34bf5942afa28c1218
srdjanStankovic <srdjanrstankovic@gmail.com>
2019-08-08 20:57:07 +0200

    black code formater

--- sim800h.py
+++ sim800h.py
@@ -1,68 +1,74 @@
 #!/usr/bin/python2
-'''
+"""
 Project	 	: SIM800 test script 
 Date&Time	: 08th August 2019.
 Description	: This is test script for SIMcom SIM800H module
 		http://simcomm2m.com/En/module/detail.aspx?id=75
-'''
+"""
 import time, sys
 import logging
 from sim800h_api import SIM800H
 
 
-COMPORT_NAME 	= "/dev/ttyUSB0"
-VERSION		= "0.0.3"
+COMPORT_NAME = "/dev/ttyUSB0"
+VERSION = "0.0.3"
+
 
 def main():
-	print("..######..####.##.....##..#######....#####.....#####...##.....##\
+    print(
+        "..######..####.##.....##..#######....#####.....#####...##.....##\
 \n\r.##....##..##..###...###.##.....##..##...##...##...##..##.....##\
 \n\r.##........##..####.####.##.....##.##.....##.##.....##.##.....##\
 \n\r..######...##..##.###.##..#######..##.....##.##.....##.#########\
 \n\r.......##..##..##.....##.##.....##.##.....##.##.....##.##.....##\
 \n\r.##....##..##..##.....##.##.....##..##...##...##...##..##.....##\
 \n\r..######..####.##.....##..#######....#####.....#####...##.....##\
-" + "\n\r\t Test module written for SIMcom SIM800H module \n\r\t\t\t   v" + VERSION)	
+"
+        + "\n\r\t Test module written for SIMcom SIM800H module \n\r\t\t\t   v"
+        + VERSION
+    )
 
-        logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
 
-        sim800h = SIM800H(portName=COMPORT_NAME)
-	sim800h.openComPort()
+    sim800h = SIM800H(portName=COMPORT_NAME)
+    sim800h.openComPort()
 
+    if not sim800h.checkCommunication():
+        logging.error("Couldn't communicate with module")
+        sys.exit()
 
-	if not sim800h.checkCommunication():
-		logging.error("Couldn't communicate with module")
-		sys.exit()
+    while 1:
+        print(
+            "\n\r-----------------\n\rChoose operation:\n\r1 - Send SMS\n\r2 - Call\n\rq - Quit program\n\r-----------------"
+        )
+        try:
+            in_selection = ""
+            in_selection = raw_input(">> ")
+        except Exception as e:
+            logging.debug("Error: " + str(e))
+            break
+        else:
+            if in_selection == "1":
+                if sim800h.sendSms():
+                    print("SMS sent successfully")
+                else:
+                    logging.error("Send SMS")
+            elif in_selection == "2":
+                if sim800h.call():
+                    print("Successfully ended call")
+                else:
+                    logging.error("To Make a call")
 
-	while 1:
-		print("\n\r-----------------\n\rChoose operation:\n\r1 - Send SMS\n\r2 - Call\n\rq - Quit program\n\r-----------------" )
-		try:
-		    in_selection = ''
-		    in_selection = raw_input('>> ')
-		except Exception as e:
-		    logging.debug('Error: ' + str(e))
-		    break
-		else:
-		    if in_selection == '1':
-			    if sim800h.sendSms():
-			        print("SMS sent successfully")
-                            else:
-			        logging.error("Send SMS")
-		    elif in_selection == '2':	                
-			    if sim800h.call():
-	            	        print("Successfully ended call")
-                            else:
-				 logging.error("To Make a call")
-
-		    elif in_selection == 'q':
-			    sim800h.closeComPort()
-			    print("Thank you for using SIM800H app")
-			    time.sleep(0.3)
-			    sys.exit()
-		    else:
-			    print("Wrong selection.\n")
+            elif in_selection == "q":
+                sim800h.closeComPort()
+                print("Thank you for using SIM800H app")
+                time.sleep(0.3)
+                sys.exit()
+            else:
+                print("Wrong selection.\n")
 
 
 if __name__ == "__main__":
-	main()
-	print("End of module")
-#EOF
+    main()
+    print("End of module")
+# EOF
--- sim800h_api.py
+++ sim800h_api.py
@@ -1,111 +1,127 @@
 #!/usr/bin/python2
-'''
+"""
 Project	 	: SIM800 test script 
 Date&Time	: 08th August 2019.
 Description	: This module consists all API's nececeary for testing SIMcom SIM800H module
 		http://simcomm2m.com/En/module/detail.aspx?id=75
-'''
+"""
 import serial
 import logging
 import time, sys, codecs
 
+
 class SIM800H:
-	def __init__(self, portName="", baudRate=115200, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, timeout=5):
-		self.portName = portName
-		self.baudRate = baudRate
-		self.bytesize = bytesize
-		self.parity = parity
-		self.stopbits = stopbits
-		self.timeout = timeout
+    def __init__(
+        self,
+        portName="",
+        baudRate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        timeout=5,
+    ):
+        self.portName = portName
+        self.baudRate = baudRate
+        self.bytesize = bytesize
+        self.parity = parity
+        self.stopbits = stopbits
+        self.timeout = timeout
 
-	def openComPort(self):
-		try:
-			self.ser = serial.Serial(self.portName, self.baudRate, timeout=self.timeout, bytesize=self.bytesize, parity=self.parity, stopbits=self.stopbits)
-			time.sleep(0.5)
-		except:
-			logging.error("Couldn't open desired tty port: " + self.portName)
-			sys.exit()
+    def openComPort(self):
+        try:
+            self.ser = serial.Serial(
+                self.portName,
+                self.baudRate,
+                timeout=self.timeout,
+                bytesize=self.bytesize,
+                parity=self.parity,
+                stopbits=self.stopbits,
+            )
+            time.sleep(0.5)
+        except:
+            logging.error("Couldn't open desired tty port: " + self.portName)
+            sys.exit()
 
-	def closeComPort(self):
-		try:
-			self.ser.close()
-		except:
-			logging.error("Couldn't close tty port")
-			sys.exit()
+    def closeComPort(self):
+        try:
+            self.ser.close()
+        except:
+            logging.error("Couldn't close tty port")
+            sys.exit()
 
-	def sendAtCommand(self, command):
-		self.command = command
-		try:
-			self.ser.write(command + '\r')
-			received=self.ser.read(20)
-			logging.debug(received)
-			if "ERROR" in received:
-				return False		
-			return received
-		except:
-			print("Couldn't write on " + self.portName)
-			return False
+    def sendAtCommand(self, command):
+        self.command = command
+        try:
+            self.ser.write(command + "\r")
+            received = self.ser.read(20)
+            logging.debug(received)
+            if "ERROR" in received:
+                return False
+            return received
+        except:
+            print("Couldn't write on " + self.portName)
+            return False
 
-	def checkCommunication(self):
-		if not self.sendAtCommand("AT"):
-			return False
-		return True
+    def checkCommunication(self):
+        if not self.sendAtCommand("AT"):
+            return False
+        return True
 
-	def sendSms(self):
-		try:
-		    number = raw_input('To >> ')
-		except Exception as e:
-		    logging.error('Error: ' + str(e))
-		    return False
-		try:
-		    message = raw_input('Insert Message >> ')
-		except Exception as e:
-		    logging.error('Error: ' + str(e))
-		    return False
+    def sendSms(self):
+        try:
+            number = raw_input("To >> ")
+        except Exception as e:
+            logging.error("Error: " + str(e))
+            return False
+        try:
+            message = raw_input("Insert Message >> ")
+        except Exception as e:
+            logging.error("Error: " + str(e))
+            return False
 
-		print("\n\r...sending SMS")
-		if not self.sendAtCommand("AT+CMGF=1"):
-			logging.error("To send AT command: AT+CMGF=1")
-			return False
-		if not self.sendAtCommand("AT+CMGS=\"" + number + "\""):
-			logging.error("To send AT command: AT+CMGS=")
-			return False
-		if not self.sendAtCommand(message):
-			logging.error("To send AT command: message content")
-			return False
+        print("\n\r...sending SMS")
+        if not self.sendAtCommand("AT+CMGF=1"):
+            logging.error("To send AT command: AT+CMGF=1")
+            return False
+        if not self.sendAtCommand('AT+CMGS="' + number + '"'):
+            logging.error("To send AT command: AT+CMGS=")
+            return False
+        if not self.sendAtCommand(message):
+            logging.error("To send AT command: message content")
+            return False
 
-		if not self.sendAtCommand("1A".decode("hex")):
-			logging.error("To send AT command: Ctrl+Z")
-			return False
+        if not self.sendAtCommand("1A".decode("hex")):
+            logging.error("To send AT command: Ctrl+Z")
+            return False
 
-		return True
+        return True
 
-	def call(self):
-		try:
-		    number = raw_input('Insert Number >> ')
-		except Exception as e:
-		    logging.error(str(e))
-		    return False
+    def call(self):
+        try:
+            number = raw_input("Insert Number >> ")
+        except Exception as e:
+            logging.error(str(e))
+            return False
 
-		print("\n\r...processing call")
-		if not self.sendAtCommand("ATD" + number + ";"):
-			logging.error("To send AT command: ATD")
-			return False
-		if not self.sendAtCommand("ATL9"):
-			logging.error("To send AT command: ATL")
-			return False
-		if not self.sendAtCommand("ATM9"):
-			logging.error("To send AT command: ATM")
-			return False
+        print("\n\r...processing call")
+        if not self.sendAtCommand("ATD" + number + ";"):
+            logging.error("To send AT command: ATD")
+            return False
+        if not self.sendAtCommand("ATL9"):
+            logging.error("To send AT command: ATL")
+            return False
+        if not self.sendAtCommand("ATM9"):
+            logging.error("To send AT command: ATM")
+            return False
 
-		try:
-			number = raw_input('Call established press ENTER if want to END call >> ')
-		except Exception as e:
-			logging.error(str(e))
-			return False
+        try:
+            number = raw_input("Call established press ENTER if want to END call >> ")
+        except Exception as e:
+            logging.error(str(e))
+            return False
 
-		if not self.sendAtCommand("ATH"):
-			logging.error("To send AT command: ATH")
-			return False
-		
-		return True
+        if not self.sendAtCommand("ATH"):
+            logging.error("To send AT command: ATH")
+            return False
+
+        return True
